ffec2680e995f16d2ef5a6be7d28fba624e039ef
Teashrock <kajitsu22@gmail.com>
2026-09-03 19:37:36 +0300

    Next

--- db/models.py
+++ db/models.py
@@ -1,1 +1,23 @@
 from django.db import models
+
+
+class Race(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255)
+    bonus = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+
+class Guild(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+class Player(models.Model):
+    nickname = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255, unique=False)
+    bio = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    guild = models.ForeignKey(Guild, on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
