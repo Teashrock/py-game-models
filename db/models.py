09e45208d4bc603068b79f52309afc35d612dec5
Teashrock <kajitsu22@gmail.com>
2026-09-03 20:05:20 +0300

    Next

--- db/models.py
+++ db/models.py
@@ -21,6 +21,10 @@ class Player(models.Model):
     nickname = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255, unique=False)
     bio = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name="race")
-    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, related_name="guild")
+    race = models.ForeignKey(
+        Race, on_delete=models.CASCADE, related_name="race"
+    )
+    guild = models.ForeignKey(
+        Guild, on_delete=models.SET_NULL, related_name="guild"
+    )
     created_at = models.DateTimeField(auto_now_add=True)
